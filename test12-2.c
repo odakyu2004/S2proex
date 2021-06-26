7a4eef00b1b88268c0b85d8a612f449381b8d807
ShikaSan <68810944+odakyu2004@users.noreply.github.com>
2021-06-27 00:47:59 +0900

    Add files via upload

--- test12-2.c
+++ test12-2.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #define AVERAGE 14.66
 
 int main(void){
     FILE *idavefile2;
+    FILE *in_file;
     char todofukenmei[9];
+    char name[10];
     char conma[2];
     char row[50];
     int todofuken;
     float ido,kion,kionsa;
     idavefile2=fopen("idave2.prn","r"); 
-  
+    
+    printf("input file name ?"); 
+    scanf("%s", name);
+    if((in_file = fopen(name,"w"))==NULL) {
+        printf("Can not open file. \n");
+        exit(-1); 
+    }
+
+    
     printf(" ** 全国の気温と緯度 ** \n"); 
     printf(" 都道府県名  緯度    気温  気温差 \n"); 
 
@@ -19,14 +30,11 @@ int main(void){
         kionsa=kion-AVERAGE;
         printf( "   %s   %2.3f   %2.1f   %+2.2f\n",todofukenmei,ido,kion,kionsa);
         
+        fprintf(in_file,"%s %2.3f %2.1f %+2.2f\n",todofukenmei,ido,kion,kionsa);
     }
+    fclose(in_file);
+
     fclose(idavefile2);
-
-    printf("-----------------------------------\n");
-    printf("            全国の平均気温 %4.2f",AVERAGE);
-    printf("\n");
-
-    
 
     return 0;
 }
