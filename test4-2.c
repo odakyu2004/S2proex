f2de1358df363e993f6857a3624257c93533b82f
ShikaSan <68810944+odakyu2004@users.noreply.github.com>
2021-11-05 09:57:46 +0900

    Update test4-2.c

--- test4-2.c
+++ test4-2.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <ctype.h>
 
 int main(void){
+    char omosas[10];
     float omosa;
+    int i;
 
     printf("郵便物の重さ(g)を入力 ");
-    scanf("%f",&omosa);
+    scanf("%s",&omosas);
     printf("\n");
     
-    if(isdigit('omosa')){
-        printf("無効な数値です");
-    }else if(0<omosa&&omosa<=25){
-        printf("郵便料金は82円です");
-    }else if(omosa<=50&&omosa>0){
-        printf("郵便料金は92円です");
-    }else if(omosa>50){
-        printf("郵便物が重すぎます");
-    }else {
-        printf("無効な数値です");
+    for (i=0; i<strlen(omosas); i++) {
+       if (!isdigit(omosas[i])) {
+       		printf("無効な数値です");
+       		return 0; 
+       }
     }
-
+    
+    omosa=atoi(omosas);
+    
+    if(omosa<=0){
+    	printf("無効な数値です");  
+	}else if(omosa>50){
+        printf("郵便物が重すぎます");
+    }else if(0<omosa&&omosa<=25){
+       	printf("郵便料金は82円です");
+    }else if(omosa<=50&&omosa>0){
+       	printf("郵便料金は92円です");
+   	} 
+   	　
     return 0;
 }
