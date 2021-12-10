2b359d0acad923976567e984772f0cd4d6ff6649
ShikaSan <68810944+odakyu2004@users.noreply.github.com>
2021-12-10 15:57:50 +0900

    Update test10-2-1.c

--- test10-2-1.c
+++ test10-2-1.c
@@ -2,12 +2,17 @@
 
 int main(){
     float in;
+    double in2;
     float *p;
+    double *q;
     
     printf("10進数で数値を入力してください");
     scanf("%f",&in);
     p=&in;
-    printf("%x",*(unsigned int*)p);
+    in2=(double)in;
+    q=&in2;
+    printf("%x\n",*(unsigned int*)p);
+    printf("%lx\n",*(unsigned long int*)q);
     
     return 0;
 }
